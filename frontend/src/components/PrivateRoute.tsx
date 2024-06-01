2f732fccad6b6bbe0a02f287f57e6b9cbd9a5e69
Prajwal G <93521144+Prajwalg19@users.noreply.github.com>
2024-06-01 16:16:29 +0530

    Update PrivateRoute.tsx

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,6 +1,6 @@
 import {useSelector} from "react-redux";
 import {Navigate, Outlet} from "react-router-dom";
-import {RootState} from "./store";
+import {RootState} from "@/store";
 function PrivateRoute() {
     const token = useSelector((store: RootState) => store.user.token)
     return token ? <Outlet /> : <Navigate to="/login" />
