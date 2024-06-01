ba189cde26a9cedba723900e7666262cbd22689c
Prajwalg19 <theimperical8@gmail.com>
2024-06-01 20:52:18 +0530

    debugging

--- frontend/src/components/MainFeed.tsx
+++ frontend/src/components/MainFeed.tsx
@@ -86,7 +86,7 @@ export default function MainFeed() {
             }
 
         } catch (e) {
-            toast.error("Something went wrong")
+            toast.error(e)
             if (user)
                 dispatch(setTempLike({postId, userId: user._id}));
         }
